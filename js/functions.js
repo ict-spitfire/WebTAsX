9629f69ab84acbe45b40e29a7e5e83c3472166c2
Dennis Boldt <boldt@itm.uni-luebeck.de>
2013-10-14 12:24:01 +0300

    Fixed Chrome.

--- js/functions.js
+++ js/functions.js
@@ -293,3 +293,18 @@ Array.prototype.contains = function(obj) {
     }
     return false;
 }
+
+
+// @see: http://stackoverflow.com/questions/646628/javascript-startswith
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
+// @see: http://stackoverflow.com/questions/646628/javascript-startswith
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+    return this.slice(-str.length) == str;
+  };
+}
